020166f1a268367d010a6c56ac43a2a3364fc1d8
Yousef Abdallah <yousefabdallah031@gmail.com>
2026-02-12 00:33:15 +0200

    ads feature added

--- apps/web/src/app/about/page.jsx
+++ apps/web/src/app/about/page.jsx
@@ -40,6 +40,7 @@ export default function AboutPage() {
         <Footer />
       </RevealOnScroll>
     </PageFadeIn>
+    </>
   );
 }
 
--- apps/web/src/app/help/page.jsx
+++ apps/web/src/app/help/page.jsx
@@ -62,6 +62,7 @@ export default function HelpCenterPage() {
         <Footer />
       </RevealOnScroll>
     </div>
+    </>
   );
 }
 
